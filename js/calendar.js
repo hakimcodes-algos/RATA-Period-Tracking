f6d26318c9c327401cd2b0392eea216e04c0c725
hakimcodes-algos <nshimiyimanahakim7@gmail.com>
2024-07-12 18:51:06 +0200

    Update calendar.js

--- js/calendar.js
+++ js/calendar.js
@@ -1,5 +1,5 @@
 /* global dates, helpers */
-'use strict';
+"use strict";
 
 (function (window) {
   const Calendar = function () {
@@ -17,44 +17,44 @@
 
       const selectedMonthYear = dates.newDate(
         `${selectedYearN}-${selectedMonthN}`,
-        'YYYY-M'
+        "YYYY-M"
       );
-      const title = selectedMonthYear.formatDate('MMMM YYYY');
+      const title = selectedMonthYear.formatDate("MMMM YYYY");
 
       // previous and next month data for nav links
-      const nextMonth = selectedMonthYear.cloneDate().addDate(1, 'months');
-      const prevMonth = selectedMonthYear.cloneDate().addDate(-1, 'months');
+      const nextMonth = selectedMonthYear.cloneDate().addDate(1, "months");
+      const prevMonth = selectedMonthYear.cloneDate().addDate(-1, "months");
       const nextMonthObj = {
-        monthN: nextMonth.getDate('month'),
-        yearN: nextMonth.getDate('year'),
-        title: nextMonth.formatDate('MMMM YYYY'),
+        monthN: nextMonth.getDate("month"),
+        yearN: nextMonth.getDate("year"),
+        title: nextMonth.formatDate("MMMM YYYY"),
       };
       const prevMonthObj = {
         daysInMonth: prevMonth.daysInMonthDate(),
-        monthN: prevMonth.getDate('month'),
-        yearN: prevMonth.getDate('year'),
-        title: prevMonth.formatDate('MMMM YYYY'),
+        monthN: prevMonth.getDate("month"),
+        yearN: prevMonth.getDate("year"),
+        title: prevMonth.formatDate("MMMM YYYY"),
       };
 
       // previous and next year data for nav links
-      const nextYear = selectedMonthYear.cloneDate().addDate(1, 'years');
-      const prevYear = selectedMonthYear.cloneDate().addDate(-1, 'years');
+      const nextYear = selectedMonthYear.cloneDate().addDate(1, "years");
+      const prevYear = selectedMonthYear.cloneDate().addDate(-1, "years");
       const nextYearObj = {
-        monthN: nextYear.getDate('month'),
-        yearN: nextYear.getDate('year'),
-        title: nextYear.formatDate('MMMM YYYY'),
+        monthN: nextYear.getDate("month"),
+        yearN: nextYear.getDate("year"),
+        title: nextYear.formatDate("MMMM YYYY"),
       };
       const prevYearObj = {
-        monthN: prevYear.getDate('month'),
-        yearN: prevYear.getDate('year'),
-        title: prevYear.formatDate('MMMM YYYY'),
+        monthN: prevYear.getDate("month"),
+        yearN: prevYear.getDate("year"),
+        title: prevYear.formatDate("MMMM YYYY"),
       };
 
       // current month and year data for today link
       const todayObj = {
-        monthN: helpers.today.getDate('month'),
-        yearN: helpers.today.getDate('year'),
-        title: helpers.today.formatDate('MMMM YYYY'),
+        monthN: helpers.today.getDate("month"),
+        yearN: helpers.today.getDate("year"),
+        title: helpers.today.formatDate("MMMM YYYY"),
       };
 
       return {
@@ -86,15 +86,15 @@
 
       const thisMonth = dates.newDate(
         `${selectedYearN}-${selectedMonthN}`,
-        'YYYY-M'
+        "YYYY-M"
       );
-      const weekdayFirstDayThisMonth = thisMonth.getDate('day');
+      const weekdayFirstDayThisMonth = thisMonth.getDate("day");
       const daysInThisMonth = thisMonth.daysInMonthDate();
       const lastDayThisMonth = dates.newDate(
-        selectedYearN + '-' + selectedMonthN + '-' + daysInThisMonth,
-        'YYYY-M-D'
+        selectedYearN + "-" + selectedMonthN + "-" + daysInThisMonth,
+        "YYYY-M-D"
       );
-      const weekdayLastDayThisMonth = lastDayThisMonth.getDate('day');
+      const weekdayLastDayThisMonth = lastDayThisMonth.getDate("day");
 
       // before
       let diffStart = weekdayFirstDayThisMonth - startDayOfWeek;
@@ -103,30 +103,30 @@
       }
       diffStart = prevMonthObj.daysInMonth - diffStart + 1;
       for (let i = diffStart; i <= prevMonthObj.daysInMonth; i++) {
+        const dateStr =
+          prevMonthObj.yearN +
+          "-" +
+          helpers.z(prevMonthObj.monthN) +
+          "-" +
+          helpers.z(i);
         days.push({
-          date:
-            prevMonthObj.yearN +
-            '-' +
-            helpers.z(prevMonthObj.monthN) +
-            '-' +
-            helpers.z(i),
+          date: dateStr,
           n: i,
-          c: '',
-          k: ['calendar__day--another-month'],
+          c: "",
+          k: ["calendar__day--another-month"],
+          ariaLabel: dates.newDate(dateStr).formatDate("MMMM D, YYYY"),
         });
       }
       // during
       for (let i = 1; i <= daysInThisMonth; i++) {
+        const dateStr =
+          selectedYearN + "-" + helpers.z(selectedMonthN) + "-" + helpers.z(i);
         days.push({
-          date:
-            selectedYearN +
-            '-' +
-            helpers.z(selectedMonthN) +
-            '-' +
-            helpers.z(i),
+          date: dateStr,
           n: i,
-          c: '',
+          c: "",
           k: [],
+          ariaLabel: dates.newDate(dateStr).formatDate("MMMM D, YYYY"),
         });
       }
       // after
@@ -135,16 +135,18 @@
         diffEnd = 7 + diffEnd;
       }
       for (let i = 1; i <= diffEnd; i++) {
+        const dateStr =
+          nextMonthObj.yearN +
+          "-" +
+          helpers.z(nextMonthObj.monthN) +
+          "-" +
+          helpers.z(i);
         days.push({
-          date:
-            nextMonthObj.yearN +
-            '-' +
-            helpers.z(nextMonthObj.monthN) +
-            '-' +
-            helpers.z(i),
+          date: dateStr,
           n: i,
-          c: '',
-          k: ['calendar__day--another-month'],
+          c: "",
+          k: ["calendar__day--another-month"],
+          ariaLabel: dates.newDate(dateStr).formatDate("MMMM D, YYYY"),
         });
       }
 
@@ -160,7 +162,6 @@
     ) {
       var calendarData = function (data, periodLength, days) {
         // maximum date that is less than or equal the first day of the visible month
-        // TODO what is this?
         var floor;
         var firstDay = days[0].date;
         var firstDayDate = dates.newDate(firstDay);
@@ -189,19 +190,19 @@
         if (!ceiling && data.quicklist.length) {
           var lastEventDate = dates.newDate(sliceOfDates[0]);
           var daysLastSinceLastEvent =
-            lastDayDate.diffDate(lastEventDate, 'days') + 1;
+            lastDayDate.diffDate(lastEventDate, "days") + 1;
           if (daysLastSinceLastEvent > data.average) {
             var daysFirstSinceLastEvent =
-              firstDayDate.diffDate(lastEventDate, 'days') + 1;
+              firstDayDate.diffDate(lastEventDate, "days") + 1;
             var daysFirstDiff = daysFirstSinceLastEvent % data.average;
             var n = firstDayDate
               .cloneDate()
-              .subtractDate(daysFirstDiff - 1, 'days');
+              .subtractDate(daysFirstDiff - 1, "days");
             sliceOfDates = [n.formatDate(helpers.datePattern)];
             var daysEndDiff = daysFirstDiff + days.length;
             var quotient = Math.floor(daysEndDiff / data.average);
             for (var q = 0; q <= quotient; q++) {
-              n.addDate(data.average, 'days');
+              n.addDate(data.average, "days");
               sliceOfDates.unshift(n.formatDate(helpers.datePattern));
             }
           }
@@ -210,102 +211,69 @@
         var firstEventDate = dates.newDate(
           sliceOfDates[sliceOfDates.length - 1]
         );
-        var counter = firstDayDate.diffDate(firstEventDate, 'days') + 1;
+        var counter = firstDayDate.diffDate(firstEventDate, "days") + 1;
         var actual = false;
 
         var todayStr = helpers.todayStr;
+        var fertilizationStart = 11;
+        var fertilizationEnd = 17;
         days.forEach(function (item) {
           if (sliceOfDates.indexOf(item.date) !== -1) {
             counter = 1;
           }
           if (counter) {
-            // TODO check this if
             if (counter > 0) {
               item.c = counter;
             }
             counter++;
           }
           if (item.date === todayStr) {
-            item.k.push('calendar__day--today');
+            item.k.push("calendar__day--today");
           } else if (item.date > todayStr) {
-            item.k.push('calendar__day--future');
+            item.k.push("calendar__day--future");
           }
           if (item.c === 1) {
             if (data.quicklist.indexOf(item.date) !== -1) {
               actual = true;
-              item.k.push('calendar__day--selected');
+              item.k.push("calendar__day--selected");
+              item.ariaLabel += `, Period Day ${item.c}`;
             } else {
               actual = false;
-              item.k.push('calendar__day--selected-future');
+              item.k.push("calendar__day--selected-future");
+              item.ariaLabel += `, Period Day ${item.c} (Next Month)`;
             }
-          } else if (item.c > 1 && item.c <= periodLength) {
-            if (actual) {
-              item.k.push('calendar__day--selected');
-            } else {
-              item.k.push('calendar__day--selected-future');
-            }
-          } else {
-            actual = false;
+          } else if (
+            item.c >= fertilizationStart &&
+            item.c <= fertilizationEnd
+          ) {
+            item.k.push("calendar__day--fertilization");
+            item.ariaLabel += `, Possible Day for Fertilization`;
           }
         });
 
         return days;
       };
 
-      /**
-       * @param {object} data
-       * @param {number} startDayOfWeek - 0 Sunday, 1 Monday, 6 Saturday
-       * @param {number} periodLength - Length of period
-       * @param {number} selectedMonthN - Selected month number
-       * @param {number} selectedYearN - Selected year number
-       */
-      var calendarGet = (
-        data,
+      const chromeData = this._calendarChrome(
         startDayOfWeek,
-        periodLength,
-        selectedMonthN,
-        selectedYearN
-      ) => {
-        // TODO clean variable names
-        const giulia = this._calendarChrome(
-          startDayOfWeek,
-          selectedMonthN,
-          selectedYearN
-        );
-
-        let days = this._calendarDays(
-          startDayOfWeek,
-          selectedMonthN,
-          selectedYearN,
-          giulia.prevMonthObj,
-          giulia.nextMonthObj
-        );
-
-        days = calendarData(data, periodLength, days);
-
-        var res = {
-          days: days,
-          week: giulia.weekTitle,
-          title: giulia.title,
-          next: giulia.nextMonthObj,
-          prev: giulia.prevMonthObj,
-          nextYear: giulia.nextYearObj,
-          prevYear: giulia.prevYearObj,
-          current: giulia.todayObj,
-        };
-
-        return res;
-      };
-
-      return calendarGet(
-        data,
-        startDayOfWeek,
-        periodLength,
         selectedMonthN,
         selectedYearN
       );
+      const daysData = this._calendarDays(
+        startDayOfWeek,
+        selectedMonthN,
+        selectedYearN,
+        chromeData.prevMonthObj,
+        chromeData.nextMonthObj
+      );
+      const calendarDays = calendarData(data, periodLength, daysData);
+
+      return {
+        chromeData,
+        calendarDays,
+      };
     };
   };
 
-  window.calendar = new Calendar();
+  window.Calendar = Calendar;
 })(window);
